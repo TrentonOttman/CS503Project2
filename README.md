b862a17b8a866c726bab059c7a3324e62fec614f
Trenton Ottman <trentondavidottman@gmail.com>
2025-10-07 15:42:37 +0100

    4.1

---
+++ README.md
@@ -0,0 +1,2 @@
+# CS503 Project 2 Lox
+Current implementation ends at the start of 4.2
--- src/main/scala/Lox.scala
+++ src/main/scala/Lox.scala
@@ -1,15 +1,19 @@
 
 package com.craftinginterpreters.lox
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.nio.charset.Charset
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.List
+import scala.util.control.Breaks._
+import java.util.Scanner
+import scala.compiletime.ops.boolean._
 
 object Lox {
+  var hadError: Boolean = false;
   def main(args: Array[String]): Unit = {
     try {
       if (args.length > 1) {
@@ -27,7 +31,11 @@ object Lox {
 
   private def runFile(path: String): Unit = {
     try {
+      var bytes: Array[Byte] = Files.readAllBytes(Paths.get(path))
+      run(new String(bytes, Charset.defaultCharset()))
 
+      // Indicate an error in the exit code.
+      if (hadError) System.exit(65);
     } catch {
       case e: IOException => println("IOException occurred.")
     }
@@ -35,9 +43,33 @@ object Lox {
 
   private def runPrompt(): Unit = {
     try {
+      var input: InputStreamReader = new InputStreamReader(System.in)
+      var reader: BufferedReader = new BufferedReader(input)
 
+      while (true) {
+        print("> ")
+        var line: String = reader.readLine()
+        if (line == null) break
+        run(line)
+        hadError = false
+      }
     } catch {
       case e: IOException => println("IOException occurred.")
     }
+  }
+
+  private def run(source: String): Unit = {
+    var scanner: java.util.Scanner = new Scanner(source)
+    var tokens: List[String] = scanner.scanTokens()
+    tokens.forEach(println)
+  }
+
+  def error(line: Int, message: String): Unit = {
+    report(line, "", message)
+  }
+
+  private def report(line: Int, where: String, message: String): Unit = {
+    println("[line " + line + "] Error" + where + ": " + message)
+    hadError = true
   }
 }
